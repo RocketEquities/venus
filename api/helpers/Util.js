39e6518a59a3118317b577b50bad6790cf2cdf30
zander <zand3rs@gmail.com>
2017-10-26 13:01:47 +0800

    optimized Util

--- api/helpers/Util.js
+++ api/helpers/Util.js
@@ -6,39 +6,45 @@
 const _ = require("lodash");
 const crypto = require("crypto");
 
+
 module.exports = {
+  toAssetsUrl: toAssetsUrl,
+  toHash: toHash,
+  toInstanceKey: toInstanceKey
+};
 
-  toAssetsUrl: function(path) {
-    const assetsBaseUrl = _.get(sails.config, "assets.baseUrl") || "";
-    const assetsUrl = assetsBaseUrl + (path || "/");
-    return assetsUrl;
-  },
+//==============================================================================
 
-  //----------------------------------------------------------------------------
+function toAssetsUrl(path) {
+  const assetsBaseUrl = _.get(sails.config, "assets.baseUrl") || "";
+  const assetsUrl = assetsBaseUrl + (path || "/");
+  return assetsUrl;
+}
 
-  toHash: function(data, encoding) {
-    const hash = crypto.createHmac("sha256", "venus")
-                       .update(data)
-                       .digest(encoding || "hex");
-    return hash;
-  },
+//------------------------------------------------------------------------------
 
-  //----------------------------------------------------------------------------
+function toHash(data, encoding) {
+  const hash = crypto.createHmac("sha256", "venus")
+                     .update(data)
+                     .digest(encoding || "hex");
+  return hash;
+}
 
-  toInstanceKey: function(value) {
-    const self = this;
-    let key = "";
+//------------------------------------------------------------------------------
 
-    if (_.isPlainObject(value)) {
-      _.forEach(_.keys(value).sort(), function(k) {
-          key && (key += ",");
-          key += (k + ":" + self.toInstanceKey(value[k]));
-      });
-    } else {
-      key = _.toString(value);
-    }
+function toInstanceKey(value) {
+  let key = "";
 
-    return key;
+  if (_.isPlainObject(value)) {
+    _.forEach(_.keys(value).sort(), function(k) {
+        key && (key += ",");
+        key += (k + ":" + toInstanceKey(value[k]));
+    });
+  } else {
+    key = _.toString(value);
   }
 
-};
+  return key;
+}
+
+//==============================================================================
