584a007638622d8d2db67b8193a7abe9362fc225
zander <zand3rs@gmail.com>
2017-11-21 19:42:23 +0800

    sample data

---
+++ db/populate.sql
@@ -0,0 +1,11 @@
+INSERT INTO businesses (name, description, overview, created_at, updated_at) VALUES ("Business 1", "Business 1 description", "Business 1 overview", now(), now());
+INSERT INTO businesses (name, description, overview, created_at, updated_at) VALUES ("Business 2", "Business 2 description", "Business 2 overview", now(), now());
+INSERT INTO businesses (name, description, overview, created_at, updated_at) VALUES ("Business 3", "Business 3 description", "Business 3 overview", now(), now());
+
+INSERT INTO investments (user_id, business_id, amount, created_at, updated_at) VALUES (1, 1, 1000.00, now(), now());
+INSERT INTO investments (user_id, business_id, amount, created_at, updated_at) VALUES (1, 1, 2000.00, now(), now());
+INSERT INTO investments (user_id, business_id, amount, created_at, updated_at) VALUES (1, 2, 3000.00, now(), now());
+INSERT INTO investments (user_id, business_id, amount, created_at, updated_at) VALUES (1, 2, 4000.00, now(), now());
+INSERT INTO investments (user_id, business_id, amount, created_at, updated_at) VALUES (1, 3, 5000.00, now(), now());
+INSERT INTO investments (user_id, business_id, amount, created_at, updated_at) VALUES (1, 3, 6000.00, now(), now());
+
