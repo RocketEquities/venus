6d94efd2b6d2bfc3ead7b23866f346956fa9ace3
zander <zand3rs@gmail.com>
2017-11-27 11:18:53 +0800

    api for getting business list

--- api/controllers/BusinessController.js
+++ api/controllers/BusinessController.js
@@ -4,7 +4,28 @@
  */
 
 module.exports = {
-  index: (req, res) => {
-    res.apiSuccess({businesses: []});
-  }
+  index: index
 };
+
+//==============================================================================
+
+function index(req, res) {
+  function getBusinesses(next) {
+    Business.find().limit(10).exec(next);
+  }
+
+  function done(err, result) {
+    const businesses = _.get(result, "businesses") || [];
+
+    if (err) {
+      return res.apiError(err);
+    }
+
+    res.apiSuccess({ businesses: businesses });
+  }
+
+  //-- execute
+  async.series({ businesses: getBusinesses }, done);
+}
+
+//==============================================================================
